c2c9c5f3c3b3759b03078de2366943f5b1cc66e2
JefriYap12 <81524073+JefriYap12@users.noreply.github.com>
2021-06-10 23:50:27 +0700

    Add files via upload
    Belajar Migration, DB Facade, Query Builder dan Eloquent

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\Mahasiswa;
 
 class MahasiswaController extends Controller
 {
@@ -17,7 +18,8 @@ class MahasiswaController extends Controller
     public function update()
     {
         $result = DB::update('update mahasiswas set nama_mahasiswa = "Andry", updated_at = now() where npm = ?', ['1822240037']);
-        dump($result);
+        // dump($result);
+        return view('mahasiswa.index', ['all-mahasiswa' => $result]);
     }
 
     public function delete()
@@ -28,7 +30,86 @@ class MahasiswaController extends Controller
     
     public function select()
     {
-        $result = DB::select("select * from mahasiswas");
+        $kampus = "Universitas Multi Data Palembang";
+        $result = DB::select('select * from mahasiswas');
+        // dump($result);
+        return view('mahasiswa.index', ['allmahasiswa' => $result, 'kampus' => $kampus]);
+    }
+
+    public function insertQb()
+    {
+        $result = DB::table('mahasiswas')->insert(
+            [
+                'npm' => '1822240040',
+                'nama_mahasiswa' => 'didit',
+                'tempat_lahir' => 'Jakarta',
+                'tanggal_lahir' => '2000-12-11',
+                'alamat' => 'Jl_sudirman',
+                'created_at' => now()
+            ]
+        );
         dump($result);
+    }
+
+    public function updateQb()
+    {
+        $result = DB::table('mahasiswas')
+        ->where('npm', '1822240037')
+        ->update(
+            [
+                'nama_mahasiswa' => 'Jefri',
+                'updated_at' => now()
+            ]
+        );
+        dump($result);
+    }
+
+    public function deleteQb()
+    {
+        $result = DB::table('mahasiswas')
+        ->where('npm', '1822240031')
+        ->delete();
+        dump($result);
+    }
+
+    public function selectQb()
+    {
+        $kampus = "UNIVERSITAS MULTI DATA PALEMBANG";
+        $result = DB::table('mahasiswas')->get();
+        return view('mahasiswa.index', ['allmahasiswa' => $result, 'kampus' => $kampus]);
+    }
+
+    public function insertElq()
+    {
+        $mahasiswa = new Mahasiswa;
+        $mahasiswa->npm = '1822240099';
+        $mahasiswa->nama_mahasiswa = 'Zainab';
+        $mahasiswa->tempat_lahir = 'Bandung';
+        $mahasiswa->tanggal_lahir = '2020-10-6';
+        $mahasiswa->alamat = 'Bambang Utoyo';
+        $mahasiswa->save();
+        dump($mahasiswa);
+    }
+
+    public function updateElq()
+    {
+        $mahasiswa = Mahasiswa::where('npm', '1822240099')->first();
+        $mahasiswa->nama_mahasiswa = 'Khadijah';
+        $mahasiswa->save();
+        dump($mahasiswa);
+    }
+
+    public function deleteElq()
+    {
+        $mahasiswa = Mahasiswa::where('npm', '1822240037')->first();
+        $mahasiswa->delete();
+        dump($mahasiswa);
+    }
+
+    public function selectElq()
+    {
+        $kampus = "UNIVERSITAS MULTI DATA PALEMBANG";
+        $mahasiswa = Mahasiswa::all();
+        return view('mahasiswa.index', ['allmahasiswa' => $mahasiswa, 'kampus' => $kampus]);
     }
 }
